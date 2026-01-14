482fd5b84562cc020e8a913481c54a74d29ec5c1
inquisirex <rodolfo.velasco.bodart@gmail.com>
2026-01-14 03:46:43 -0600

    completed excercise number 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,49 @@
 using System;
+using System.Linq.Expressions;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("Please write your grade");
+        string grade = Console.ReadLine();
+        int gradeInt = int.Parse(grade);
+        string letter = ("NA");
+        bool passed = false;
+
+        if (gradeInt >= 90)
+        {
+            letter = ("A");
+            passed = true;
+        }
+        else if (gradeInt >= 80)
+        {
+            letter = ("B");
+            passed = true;
+        }
+        else if (gradeInt >= 70)
+        {
+            letter = ("C");
+            passed = true;
+        }
+        else if (gradeInt >= 60)
+        {
+            letter = ("D");
+            passed = false;
+        }
+        else
+        {
+            letter = ("F");
+            passed = false;
+        }
+
+        if (passed == true)
+        {
+            Console.Write($"Congratulations! you passed this class with an {letter}.");
+        }
+        else
+        {
+            Console.Write($"You did not pass this class. Your grade was {letter}");
+        }
     }
 }
