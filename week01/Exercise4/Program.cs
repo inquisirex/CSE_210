fcd147be4a2ad73c20c2bf2a33aa0349847c79b2
inquisirex <rodolfo.velasco.bodart@gmail.com>
2026-01-14 04:46:56 -0600

    Completed excercise #4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        Console.WriteLine("Hello please enter digits to this sum, enter 0 when done.");
+        List<int> numbers = new List<int>();
+        int totalSum = 0;
+        int lastNumber = -1;
+
+        //Get numbers and calculate total sum
+        while (lastNumber != 0)
+        {
+            Console.WriteLine("Please input a number: ");
+            string lasNumberStr = Console.ReadLine();
+            lastNumber = int.Parse(lasNumberStr);
+            totalSum += lastNumber;
+            if (lastNumber != 0)
+            {
+                numbers.Add(lastNumber);
+            }
+        }
+
+        // Find average //
+        
+        int numbersOfEntries = numbers.Count;
+        decimal average = (decimal)totalSum/(decimal)numbersOfEntries;
+
+        // Find largest index 
+        int largestInteger = 0;
+        foreach (int number in numbers)
+        {
+            if (number >= largestInteger)
+            {
+                largestInteger = number;
+            }
+        }
+        Console.WriteLine($"The sum of your numbers is {totalSum}");
+        Console.WriteLine($"The average of the entries is {average}");
+        Console.WriteLine($"The biggest entry is {largestInteger}");
     }
 }
