1e50b43d7decd743ed66c0ef8738f6ba7c585042
inquisirex <rodolfo.velasco.bodart@gmail.com>
2026-01-14 04:03:25 -0600

    Completed assignment number 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+
+        Console.WriteLine("Welcome to the number guessing name.");
+        int testGuess = randomGenerator.Next(1, 100);
+        int lastGuess = -1;
+
+        while (testGuess != lastGuess)
+        {
+            Console.WriteLine("Please input your guess.");
+            string lastGuessSTR = Console.ReadLine();
+            lastGuess = int.Parse(lastGuessSTR);
+
+            if (lastGuess > testGuess)
+            {
+                Console.WriteLine("Guess too high!");
+            }
+            else if (lastGuess < testGuess)
+            {
+                Console.WriteLine("Guess was too low!");
+            }
+        }
+        Console.WriteLine("You have guessed the number!");
+
     }
 }
