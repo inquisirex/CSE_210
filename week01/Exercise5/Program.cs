b7ffec515a9b52a736b09f5690fead589ee41266
inquisirex <rodolfo.velasco.bodart@gmail.com>
2026-01-14 05:29:20 -0600

    Completed excercise 5

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,9 +1,48 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+        string finalName = UserName();
+        int finalNum = FavoriteNumber();
+        int finalSquaredNum = SquaredNumber(finalNum);
+        string finalDisplay = DisplayResult(finalName, finalSquaredNum);
+        Console.WriteLine(finalDisplay);
+    }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string UserName()
+    {
+        Console.WriteLine("Please input your name and last name");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static int FavoriteNumber()
+    {
+        Console.WriteLine("Please input your favorite number");
+        string strNumber = Console.ReadLine();
+        int number = int.Parse(strNumber);
+        return number;
+    }
+
+    static int SquaredNumber(int number)
+    {
+        int squaredNumber = number * number;
+        return squaredNumber;
+    }
+
+    static string DisplayResult(string name, int number)
+    {
+        string strNumber = number.ToString();
+        string finalString = $"{name}, the square of your number is {strNumber}";
+        return finalString;
     }
 }
